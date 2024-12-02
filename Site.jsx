ef1b9b9b4c522a8dcdbbb54a5672a80d196d3789
Lara-Fsantos <contact.laraf@gmail.com>
2024-12-02 19:12:09 -0300

    Create Site.jsx

---
+++ Site.jsx
@@ -0,0 +1,503 @@
+import React from "react";
+import "./style.css";
+
+export const Site = () => {
+  return (
+    <div className="SITE">
+      <div className="div">
+        <div className="CHS">
+          <img
+            className="captura-de-tela"
+            alt="Captura de tela"
+            src="https://c.animaapp.com/tBzEnfpL/img/captura-de-tela-2024-11-28-115905@2x.png"
+          />
+
+          <p className="text-wrapper">
+            Ao longo do ensino médio, matérias como História, Geografia,
+            Filosofia e Sociologia tiveram um papel crucial na expansão do meu
+            entendimento sobre a sociedade e o mundo em que habitamos. Com essas
+            matérias, consegui compreender de forma mais aprofundada o passado,
+            examinar a estrutura das sociedades e ponderar sobre questões
+            filosóficas e sociais que influenciam nosso cotidiano. As minhas
+            atividades preferidas nessas matérias incluíam a pesquisa e
+            avaliação crítica de acontecimentos históricos, a condução de
+            discussões e a exploração das diversas escolas filosóficas. Essas
+            tarefas contribuíram para o aprimoramento do meu raciocínio crítico
+            e a compreensão do comportamento humano ao longo do tempo.
+          </p>
+
+          <div className="ci-ncias-humanas">
+            Ciências
+            <br />
+            Humanas
+          </div>
+        </div>
+
+        <div className="matemtica">
+          <img
+            className="download"
+            alt="Download"
+            src="https://c.animaapp.com/tBzEnfpL/img/download@2x.png"
+          />
+
+          <p className="p">
+            As minhas tarefas favoritas nessas matérias eram as atividades
+            práticas de solução de problemas, particularmente quando podíamos
+            aplicar os conceitos em contextos cotidianos, tornando o aprendizado
+            mais tangível e aplicável.
+          </p>
+
+          <img
+            className="fundo-de-frmula"
+            alt="Fundo de frmula"
+            src="https://c.animaapp.com/tBzEnfpL/img/fundo-de-f-rmula-matem-tica-de-educa--o-de-s-mbolo-de-n-mero-no-@2x.png"
+          />
+
+          <p className="text-wrapper-2">
+            As matérias de Matemática e Estatística que estudei no ensino médio
+            foram essenciais para o aprimoramento do meu pensamento lógico e
+            quantitativo. A solução de problemas matemáticos, a aprendizagem de
+            álgebra e geometria, além da análise de dados na Estatística, me
+            auxiliaram a compreender a presença dos números e fórmulas no dia a
+            dia e em várias áreas do saber.
+          </p>
+
+          <div className="text-wrapper-3">Matemática</div>
+        </div>
+
+        <div className="IOT">
+          <p className="text-wrapper-4">
+            Organização dos Armários e Materiais de IOT
+          </p>
+
+          <img
+            className="image"
+            alt="Image"
+            src="https://c.animaapp.com/tBzEnfpL/img/image--3-.png"
+          />
+
+          <p className="durante-o-meu-tempo">
+            Durante o meu tempo no SENAC, a disciplina de Internet das Coisas
+            foi, sem dúvida, a minha favorita. A oportunidade de explorar como
+            diferentes dispositivos podem ser conectados e interagir entre si
+            por meio de sensores, redes e programação foi uma experiência
+            fascinante. IoT me permitiu entender como a tecnologia está
+            transformando o mundo ao nosso redor e como podemos aplicar esses
+            conhecimentos para criar soluções inovadoras. O aprendizado prático,
+            envolvendo a construção de projetos e a programação de dispositivos,
+            foi o que mais me motivou. Ao longo do curso, tive a chance de
+            trabalhar com vários projetos empolgantes que me ajudaram a
+            desenvolver as minhas habilidades.
+            <br /> Obrigada glauco por todo apoio e carinho, sem você não teria
+            conseguido.
+          </p>
+
+          <div className="text-wrapper-5">Atividade do Carrinho Arduino</div>
+
+          <div className="text-wrapper-6">Atividade do Boné</div>
+
+          <div className="text-wrapper-7">Vídeo da Turma</div>
+
+          <div className="text-wrapper-8">Internet das Coisas</div>
+
+          <img
+            className="img"
+            alt="Captura de tela"
+            src="https://c.animaapp.com/tBzEnfpL/img/captura-de-tela-2024-11-28-122052.png"
+          />
+
+          <img
+            className="captura-de-tela-2"
+            alt="Captura de tela"
+            src="https://c.animaapp.com/tBzEnfpL/img/captura-de-tela-2024-11-28-121630.png"
+          />
+
+          <a
+            href="https://youtu.be/Ikvs6B2591M?si=WG4N9_6R1RoWHhjT"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <img
+              className="captura-de-tela-3"
+              alt="Captura de tela"
+              src="https://c.animaapp.com/tBzEnfpL/img/captura-de-tela-2024-11-28-122222.png"
+            />
+          </a>
+        </div>
+
+        <div className="CNT">
+          <div className="overlap-group">
+            <p className="text-wrapper-9">
+              As disciplinas de Ciências da Natureza, como Física, Química e
+              Biologia, me proporcionaram uma compreensão mais profunda dos
+              processos naturais e das leis que regem o universo. Através de
+              experimentos e atividades práticas, pude aplicar conceitos
+              teóricos e observar fenômenos reais, o que tornou o aprendizado
+              mais dinâmico e interessante. Minhas tarefas favoritas incluíam os
+              experimentos de laboratório, especialmente em Química, onde pude
+              explorar reações químicas, e os estudos em Biologia, onde
+              investiguei o funcionamento dos organismos e os ecossistemas.
+              Essas atividades foram essenciais para desenvolver meu raciocínio
+              científico.
+            </p>
+
+            <div className="ci-ncias-da-natureza">
+              Ciências
+              <br />
+              da Natureza
+            </div>
+
+            <img
+              className="captura-de-tela-4"
+              alt="Captura de tela"
+              src="https://c.animaapp.com/tBzEnfpL/img/captura-de-tela-2024-11-28-115351@2x.png"
+            />
+          </div>
+        </div>
+
+        <div className="linguagens">
+          <div className="overlap-group">
+            <p className="nas-mat-rias-de">
+              Nas matérias de Linguagens, que incluem Português, Inglês, Artes e
+              Educação Física, tive a oportunidade de desenvolver minha
+              criatividade e aprimorar minha habilidade de comunicação. A
+              compreensão e leitura de textos em Português e Inglês contribuíram
+              para o aumento do meu vocabulário e para o aprimoramento da minha
+              capacidade de expressão. Por outro lado, as aulas de Artes me
+              possibilitaram explorar variadas formas de expressão artística e
+              entender sua relevância na cultura. No âmbito da Educação Física,
+              as atividades práticas e esportivas serviram para manter a minha
+              saúde e adquirir conhecimento sobre o trabalho em grupo. As minhas
+              atividades preferidas nessas matérias eram as apresentações e
+              criações criativas, como projetos artísticos e competições
+              esportivas.
+            </p>
+
+            <div className="text-wrapper-10">Linguagens e Códigos</div>
+
+            <img
+              className="captura-de-tela-5"
+              alt="Captura de tela"
+              src="https://c.animaapp.com/tBzEnfpL/img/captura-de-tela-2024-11-28-114921@2x.png"
+            />
+          </div>
+        </div>
+
+        <div className="overlap">
+          <div className="works">
+            <div className="overlap-2">
+              <div className="overlap-3">
+                <div className="overlap-group-wrapper">
+                  <div className="overlap-group-2">
+                    <div className="rectangle" />
+
+                    <div className="text-wrapper-11">Ciências da Natureza</div>
+                  </div>
+                </div>
+
+                <div className="overlap-wrapper">
+                  <div className="div-wrapper">
+                    <div className="text-wrapper-12">Ciências da Humanas</div>
+                  </div>
+                </div>
+
+                <div className="linguagens-2">
+                  <div className="overlap-4">
+                    <div className="text-wrapper-13">Linguagens e Códigos</div>
+                  </div>
+                </div>
+
+                <div className="matemtica-2">
+                  <div className="overlap-5">
+                    <div className="text-wrapper-14">Matemática</div>
+                  </div>
+                </div>
+
+                <div className="IOT-2">
+                  <div className="overlap-6">
+                    <div className="text-wrapper-15">Internet das Coisas</div>
+                  </div>
+                </div>
+              </div>
+
+              <p className="text-wrapper-16">
+                Os três anos em que estudei no SENAC, tive a oportunidade de
+                realizar diversos trabalhos e aprender muitas coisas nas mais
+                variadas áreas do conhecimento. Cada atividade contribuiu para o
+                meu crescimento acadêmico e profissional, proporcionando uma
+                visão mais ampla e aprofundada sobre os temas abordados. Ao
+                longo desse período, desenvolvi habilidades práticas e teóricas
+                que me ajudaram a me preparar para diversos desafios. Abaixo,
+                vou compartilhar um pouco de cada uma dessas experiências,
+                destacando o que aprendi e como elas me impactaram.
+              </p>
+
+              <div className="text-wrapper-17">Durante o SENAC...</div>
+            </div>
+          </div>
+
+          <div className="rectangle-2" />
+
+          <div className="about-me">
+            <div className="overlap-7">
+              <div className="rectangle-3" />
+
+              <img
+                className="rectangle-4"
+                alt="Rectangle"
+                src="https://c.animaapp.com/tBzEnfpL/img/rectangle-12.svg"
+              />
+
+              <img
+                className="rectangle-5"
+                alt="Rectangle"
+                src="https://c.animaapp.com/tBzEnfpL/img/rectangle-11.svg"
+              />
+
+              <div className="text-wrapper-18">RESUMO</div>
+
+              <div className="text-wrapper-19">RESUMO</div>
+
+              <div className="text-wrapper-20">RESUMO</div>
+
+              <p className="text-wrapper-21">
+                Sou Lara Ferreira, tenho 17 anos e atualmente estou cursando
+                IoT. Meu objetivo é fazer ciência da computação e me tornar
+                programadora, unindo minha paixão por tecnologia e inovação.
+                Além disso, sou artista, e a arte é minha forma de expressar
+                ideias e explorar o mundo de maneira criativa.
+              </p>
+
+              <p className="text-wrapper-22">
+                Assistente de Suporte e Manutenção de Computadores
+              </p>
+
+              <div className="text-wrapper-23">2022</div>
+
+              <p className="text-wrapper-24">
+                Assistente de Suporte em Automação IoT
+              </p>
+
+              <p className="text-wrapper-25">
+                Assistente de Desenvolvimento de Aplicativos para IoT
+              </p>
+
+              <div className="text-wrapper-26">Intermediário</div>
+
+              <div className="text-wrapper-27">Língua materna</div>
+
+              <div className="text-wrapper-28">2024</div>
+
+              <div className="text-wrapper-29">2023</div>
+
+              <div className="oi-eu-sou-a-lara">
+                Oi, eu sou
+                <br />
+                a&nbsp;&nbsp;Lara!
+              </div>
+
+              <div className="text-wrapper-30">Educação</div>
+
+              <div className="text-wrapper-31">Línguas</div>
+
+              <div className="hobbies-interesses">Hobbies &amp; Interesses</div>
+
+              <div className="text-wrapper-32">Português</div>
+
+              <div className="text-wrapper-33">Artes</div>
+
+              <div className="text-wrapper-34">Inglês</div>
+
+              <div className="text-wrapper-35">Leitura</div>
+
+              <div className="text-wrapper-36">Robótica</div>
+
+              <div className="rectangle-6" />
+
+              <div className="rectangle-7" />
+
+              <div className="rectangle-8" />
+
+              <img
+                className="rectangle-9"
+                alt="Rectangle"
+                src="https://c.animaapp.com/tBzEnfpL/img/rectangle-7.svg"
+              />
+
+              <div className="rectangle-10" />
+
+              <a
+                className="text-wrapper-37"
+                href="https://www.canva.com/design/DAFyxtZrOEo/FcU1aNNpIBpEWafZu6eyWg/edit?utm_content=DAFyxtZrOEo&amp;utm_campaign=designshare&amp;utm_medium=link2&amp;utm_source=sharebutton"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                CV - Lara Ferreira
+              </a>
+
+              <img
+                className="image-2"
+                alt="Image"
+                src="https://c.animaapp.com/tBzEnfpL/img/image-8@2x.png"
+              />
+
+              <img
+                className="image-3"
+                alt="Image"
+                src="https://c.animaapp.com/tBzEnfpL/img/image-8@2x.png"
+              />
+
+              <a
+                className="text-wrapper-38"
+                href="https://www.linkedin.com/in/lara-ferreira-328b67298/"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                @laraferreira
+              </a>
+
+              <div className="text-wrapper-39">Maio 07, 2007</div>
+
+              <div className="text-wrapper-40">Brasileira</div>
+
+              <img
+                className="IMG"
+                alt="Img"
+                src="https://c.animaapp.com/tBzEnfpL/img/img-1368@2x.png"
+              />
+
+              <div className="rectangle-11" />
+
+              <div className="text-wrapper-41">CONTATO</div>
+
+              <div className="text-wrapper-42">São Paulo, Brasil</div>
+
+              <div className="text-wrapper-43">(11)95950-5440</div>
+
+              <img
+                className="image-4"
+                alt="Image"
+                src="https://c.animaapp.com/tBzEnfpL/img/image-3@2x.png"
+              />
+
+              <img
+                className="image-5"
+                alt="Image"
+                src="https://c.animaapp.com/tBzEnfpL/img/image-2@2x.png"
+              />
+
+              <img
+                className="image-6"
+                alt="Image"
+                src="https://c.animaapp.com/tBzEnfpL/img/image-1@2x.png"
+              />
+
+              <a
+                className="text-wrapper-44"
+                href="mailto:Contact.laraf@gmail.com"
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                Contact.laraf@gmail.com
+              </a>
+
+              <div className="rectangle-12" />
+
+              <p className="text-wrapper-45">Java, SQL, HTML, CSS, C++</p>
+
+              <div className="text-wrapper-46">Redes e Informática</div>
+
+              <p className="text-wrapper-47">Word, Power Point e Excel</p>
+
+              <div className="text-wrapper-48">Conhecimento Básico em:</div>
+
+              <div className="text-wrapper-49">Conhecimento Moderado em:</div>
+
+              <div className="text-wrapper-50">Conhecimento Avançado em:</div>
+
+              <div className="text-wrapper-51">Habilidades técnicas</div>
+
+              <img
+                className="line"
+                alt="Line"
+                src="https://c.animaapp.com/tBzEnfpL/img/line-3.svg"
+              />
+
+              <img
+                className="line-2"
+                alt="Line"
+                src="https://c.animaapp.com/tBzEnfpL/img/line-3.svg"
+              />
+
+              <img
+                className="line-3"
+                alt="Line"
+                src="https://c.animaapp.com/tBzEnfpL/img/line-3.svg"
+              />
+
+              <img
+                className="image-7"
+                alt="Image"
+                src="https://c.animaapp.com/tBzEnfpL/img/image-6@2x.png"
+              />
+
+              <img
+                className="image-8"
+                alt="Image"
+                src="https://c.animaapp.com/tBzEnfpL/img/image-5@2x.png"
+              />
+
+              <img
+                className="image-9"
+                alt="Image"
+                src="https://c.animaapp.com/tBzEnfpL/img/image-4@2x.png"
+              />
+
+              <div className="ellipse" />
+
+              <div className="scroll-down">
+                scroll <br />
+                down
+              </div>
+            </div>
+          </div>
+
+          <div className="pgina-inicial">
+            <div className="overlap-8">
+              <div className="rectangle-13" />
+
+              <div className="text-wrapper-52">PORTFOLIO</div>
+
+              <div className="text-wrapper-53">PORTFOLIO</div>
+
+              <div className="text-wrapper-54">PORTFOLIO</div>
+
+              <div className="ellipse-2" />
+
+              <div className="scroll-down-2">
+                scroll <br />
+                down
+              </div>
+            </div>
+
+            <div className="navbar">
+              <div className="text-wrapper-55">About me</div>
+
+              <div className="text-wrapper-56">Home</div>
+
+              <div className="text-wrapper-57">Works</div>
+
+              <div className="text-wrapper-58">Do you like it?</div>
+
+              <img
+                className="captura-de-tela-6"
+                alt="Captura de tela"
+                src="https://c.animaapp.com/tBzEnfpL/img/captura-de-tela-2024-11-27-165640@2x.png"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
